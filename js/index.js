e134976ef195d4c203dddd9ae90b510535899213
JoelNieto <joenieto90@hotmail.com>
2020-08-21 15:21:24 -0500

    Creating the nav structure and creating the style.scss file

---
+++ js/index.js
@@ -0,0 +1,13 @@
+<script src="js/index.js"></script>
+
+$(function() { 
+    $(".toggle").on("click", function() {
+      if ($(".item").hasClass("active")) {
+        $(".item").removeClass("active");
+        $(this).find("a").html("<i class='material-icons'>menu</i>");
+        } else {
+          $(".item").addClass("active");
+          $(this).find("a").html("<i class='material-icons'>clear</i>");
+        }
+      });
+    });
